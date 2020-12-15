5368cf16e9a1f70fd1b7bb311499f0018b7a3404
Anton <651299847@qq.com>
2020-12-15 12:55:13 +0800

    环形

---
+++ 环形判断/环形判断/源.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     struct ListNode *next;
+* };
+*/
+bool hasCycle(struct ListNode *head)
+{
+	struct ListNode* fast = head;
+	struct ListNode* slow = head;
+	while (fast&&fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == slow)
+		{
+			return true;
+		}
+	}
+	return false;
+}
