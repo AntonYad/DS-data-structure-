0f67ba701ef186bffd8ea024f83609750465c5b4
Anton <651299847@qq.com>
2021-01-01 14:16:37 +0800

    signal

--- signal.c
+++ signal.c
@@ -15,14 +15,9 @@ printf("I have got SIGQUIT\n");
 }
 int main()
 {
-struct sigaction action;
-sigaction(SIGINT,0,&action);
-action.sa_handler=my_fun;
-sigaction(SIGINT,&action,0);
-sigaction(SIGQUIT,0,&action);
-action.sa_handler=my_fun;
-sigaction(SIGQUIT,&action,0);
 printf("Waiting for signal SIGINT or SIGQUIT...\n");
+signal(SIGINT,my_fun);
+signal(SIGQUIT,my_fun);
 pause();
 exit(0);
 }
